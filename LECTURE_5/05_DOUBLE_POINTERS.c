dfe387b90ecaa366030466d1e04592380102af69
krishjais98 <preetijaiswal983@gmail.com>
2025-06-26 12:33:34 +0530

    d

--- LECTURE_5/05_DOUBLE_POINTERS.c
+++ LECTURE_5/05_DOUBLE_POINTERS.c
@@ -5,8 +5,8 @@ int main(){
     int* a=&n;   //int* is a container that is able to store address of another contaner
     int** b=&a;
     printf("%d\n",**b);
-    printf("%p\n",&n);
-    printf("%p\n",a);
-    printf("%p\n",b);
+    printf("%p\n",&n);   //yaha per n ka address 
+    printf("%p\n",a);    //yaha per n ka address a me store hai
+    printf("%p\n",b);    //yaha per a ka address store hai b me
     return 0;
 }
